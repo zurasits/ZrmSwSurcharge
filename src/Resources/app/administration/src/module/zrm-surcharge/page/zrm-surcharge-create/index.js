1679c203e42be5e2eb29444d16f8e3447f4864ac
zmanagadze <zurasits@gmail.com>
2024-08-09 00:51:42 +0200

    tmp changes

--- src/Resources/app/administration/src/module/zrm-surcharge/page/zrm-surcharge-create/index.js
+++ src/Resources/app/administration/src/module/zrm-surcharge/page/zrm-surcharge-create/index.js
@@ -20,17 +20,7 @@ export default {
 
         onClickSave() {
             this.isLoading = true;
-
-            this.repository.save(this.selectedProduct, Shopware.Context.api).then(() =>{
-                this.isLoading = false;
-                this.$router.push({ name: "zrm.surcharge.detail", params: {id: this.bundle.id} });
-            }).catch((exception) => {
-                this.isLoading = false;
-                this.createNotificationError({
-                    title: this.$tc('zrm-surcharge.detail.errorTitle'),
-                    message: exception
-                })
-            })
+            //todo save logic here
         }
     }
 
